7d176818ab280d411ef9f068972b48f6f05dd69d
SheldonGomel <sheldongomel@gmail.com>
2025-04-13 02:54:47 +0300

    Use elasticbeanstalk for cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
   product: "https://p995oywed8.execute-api.eu-west-1.amazonaws.com/dev",
-  order:
-    "https://iyfe3hlxxemjz4iwmorqjteuwy0ocjfu.lambda-url.eu-west-1.on.aws/api",
+  order: "http://sheldongomel-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
   import: "https://ps8dwx2wlg.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://iyfe3hlxxemjz4iwmorqjteuwy0ocjfu.lambda-url.eu-west-1.on.aws/api",
+  cart: "http://sheldongomel-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
