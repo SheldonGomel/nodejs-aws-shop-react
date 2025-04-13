be97ee5be255a7b4d330d85f93bb65360288480f
SheldonGomel <sheldongomel@gmail.com>
2025-04-13 14:28:40 +0300

    Add CloudFront distribution for EBS

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://p995oywed8.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "http://sheldongomel-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
+  order: "https://d2mqrjlfw56el5.cloudfront.net/api",
   import: "https://ps8dwx2wlg.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://sheldongomel-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://d2mqrjlfw56el5.cloudfront.net/api",
 };
 
 export default API_PATHS;
